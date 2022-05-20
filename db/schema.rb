f86c0b548de1c820f5cc592600484d42081843df
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-05-20 19:28:48 +0900

    新規登録機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2022_05_20_071753) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_furigana", null: false
