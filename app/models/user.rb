c65a1435c9f868c6aaa171f1b902d305c9fde370
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-05-21 15:14:50 +0900

    「ヶ」の名前が付く方にも対応できるようにバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)\w{6,}\z/i }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/ }
-  validates :last_name_furigana, presence: true, format: { with: /\A[ァ-ンー]+\z/ }
-  validates :first_name_furigana, presence: true, format: { with: /\A[ァ-ンー]+\z/ }
+  validates :last_name_furigana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_furigana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
   validates :birth_date, presence: true
 end
