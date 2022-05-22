9a8d56de432d897cffe3b58e8837e980e115cecf
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-05-23 08:35:42 +0900

    データの定義

--- db/migrate/20220522004112_create_items.rb
+++ db/migrate/20220522004112_create_items.rb
@@ -3,7 +3,13 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :item_name, null: false
       t.text :product_description, null: false
-
+      t.integer :category_id, null:false
+      t.integer :item_condition_id, null: false
+      t.integer :postage_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :shipping_time_id, null: false
+      t.integer :price, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,16 @@ ActiveRecord::Schema.define(version: 2022_05_22_004112) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.text "product_description", null: false
+    t.integer "category_id", null: false
+    t.integer "item_condition_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_time_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_05_22_004112) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
