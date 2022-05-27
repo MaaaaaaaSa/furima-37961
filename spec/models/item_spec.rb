94101dc29cc7fc9d834b58ac9aa26782e20aa788
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-05-27 15:04:59 +0900

    Rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -111,7 +111,7 @@ RSpec.describe Item, type: :model do
       it 'userが紐付いていない場合出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
