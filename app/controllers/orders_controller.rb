8219154fb7faeb54ca360a418063cab0f21064a7
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-06-05 17:14:17 +0900

    authenticate_user!を使用する記述に修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,11 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: :index
 
   def index
     @order_deliverie = OrderDeliverie.new
-    if user_signed_in?
-      if @item.order.present? || current_user.id == @item.user_id
+    if @item.order.present? || current_user.id == @item.user_id
       redirect_to root_path
-      end
-    else
-      redirect_to user_session_path
     end
   end
 
