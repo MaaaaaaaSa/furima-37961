60acba587eaffc1747fe5aaab6d4db1015d1a75b
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-06-05 16:50:23 +0900

    同じ記述をbefore_actionにまとめる修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
     @order_deliverie = OrderDeliverie.new
-    @item = Item.find(params[:item_id])
     if user_signed_in?
       if @item.order.present? || current_user.id == @item.user_id
       redirect_to root_path
@@ -12,7 +13,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_deliverie = OrderDeliverie.new(order_params)
     if @order_deliverie.valid?
       pay_item
@@ -29,6 +29,10 @@ class OrdersController < ApplicationController
     params.require(:order_deliverie).permit(:postal_code, :prefecture_id, :citys, :address, :building_name, :phone).merge(
       user_id: current_user.id, item_id: @item.id, token: params[:token]
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
   def pay_item
